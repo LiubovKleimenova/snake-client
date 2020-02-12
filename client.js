a2bc05c81e61c68a5836402469cceabe5d816d4e
LiubovKleimenova <kleimenovaliubov@gmail.com>
2020-02-12 18:28:28 +0000

    added reply from client

--- client.js
+++ client.js
@@ -10,6 +10,14 @@ const connect = function() {
   conn.on("data", data => {
     console.log("Server says: ", data);
   });
+
+  conn.on("connect", (name) => {
+    console.log("Successfully connected to game server");
+  });
+
+  conn.on("connect", () => {
+    conn.write("Name: LKS");
+  });
   return conn;
 };
 
